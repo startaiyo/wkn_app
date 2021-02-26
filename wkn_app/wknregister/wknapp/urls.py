45dd047dc5e60c4bbbda2f1e96014bd1340dfd50
startaiyo <startaiyo0104@gmail.com>
2021-02-26 11:45:32 +0900

    create login

--- wkn_app/wknregister/wknapp/urls.py
+++ wkn_app/wknregister/wknapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name ='home')
+    path('login/', views.MyLoginView.as_view(), name="login"),
+    path('logout/', views.MyLogoutView.as_view(), name="logout"),
+    path('',views.index,name ='home'),
+    path('create/', views.UserCreateView.as_view(),name="create"),
 ]
--- wkn_app/wknregister/wknapp/views.py
+++ wkn_app/wknregister/wknapp/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import json
 import sys # ErrorLog出力用
 sys.path.append('/Users/startaiyo/opt/anaconda3/lib/python3.8/site-packages')
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import os
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm  # 追記
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from . import forms
 
+class MyLoginView(LoginView):
+    form_class = forms.LoginForm
+    template_name = "wknapp/login.html"
+
+class MyLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = "wknapp/logout.html"
+
+class IndexView(TemplateView):
+    template_name = "wknapp/index.html"
+
+class UserCreateView(CreateView):
+    form_class = UserCreationForm
+    template_name = "wknapp/create.html"
+    success_url = reverse_lazy("login")
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+        
 def index(request):
     context={'equipments':{"apple":"170","banana":"178"}}
     if request.method=="GET":
@@ -17,7 +50,10 @@ def index(request):
     else:
         title = request.POST.get("title").split(',')
         many = request.POST.get("many")
+        username=request.POST.get("username")
         title.append(many)
+        title.append(username)
+        print(title)
         scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
 
         #認証情報設定
